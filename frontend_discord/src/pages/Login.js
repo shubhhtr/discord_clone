3a16f9496f0a38e8d2c2f0ee2a115c61d9ae3eff
Shubham Tripathi <shubhhtr@gmail.com>
2023-06-10 13:26:40 +0530

    Added login UI

--- frontend_discord/src/pages/Login.js
+++ frontend_discord/src/pages/Login.js
@@ -1,28 +1,81 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-	return (
-		<div className='login-wrapper'>
-			<div className='auth-box'>
-				<h1 className='heading'>
-					<span className='heading-primary'>Welcome back!</span>
-					<span className='heading-secondary'>
-						We're so excited to see you again!
-					</span>
-				</h1>
+    const [cred, setCred] = useState({});
 
-				<div className='login-form-wrapper'>
-					<label className='text-input-label'>
-						EMAIL OR PHONE NUMBER
-					</label>
-					<input
-						className='text-input-box'
-						type='text'
-					/>
-				</div>
-			</div>
-		</div>
-	);
+    const onChangeHandler = (e, k) => {
+        setCred({
+            ...cred,
+            [k]: e.target.value,
+        });
+    };
+
+    const onSubmitHandler = () => {
+        console.log(cred);
+    };
+
+    return (
+        <div className='login-wrapper'>
+            <div className='auth-box'>
+                <h1 className='heading'>
+                    <span className='heading-primary'>Welcome back!</span>
+                    <span className='heading-secondary'>
+                        We're so excited to see you again!
+                    </span>
+                </h1>
+
+                <div className='login-form-wrapper'>
+                    <label className='text-input-label wrong-cred'>
+                        EMAIL OR PHONE NUMBER
+                        <span className='wrong-cred warn-hidden'>
+                            {' '}
+                            - Login or password is invalid.
+                        </span>
+                    </label>
+                    <input
+                        className='text-input-box'
+                        type='text'
+                        onChange={(e) => onChangeHandler(e, 'email')}
+                    />
+                    <label className='text-input-label wrong-cred'>
+                        PASSWORD
+                        <span className='wrong-cred warn-hidden'>
+                            {' '}
+                            - Login or password is invalid.
+                        </span>
+                    </label>
+                    <input
+                        className='text-input-box'
+                        type='password'
+                        onChange={(e) => onChangeHandler(e, 'password')}
+                    />
+
+                    <Link
+                        to={'#'}
+                        className='forgot-password router-link'>
+                        Forgot your password?
+                    </Link>
+
+                    <button
+                        className='login-button'
+                        onClick={onSubmitHandler}>
+                        Log In
+                    </button>
+                </div>
+                <div className='registration-nav-button'>
+                    <span className='registration-nav-button-pri'>
+                        Need an account?
+                    </span>
+                    <Link
+                        to={'/registor'}
+                        className='registration-nav-button-sec router-link'>
+                        Register
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
